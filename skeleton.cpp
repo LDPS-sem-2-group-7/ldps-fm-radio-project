bb568c7e926003db5871dde915d4031863e839df
Awes Mubarak <contact@awesmubarak.com>
2021-02-26 11:14:49 +0000

    Update the logic for the frequency switching

--- skeleton.cpp
+++ skeleton.cpp
@@ -10,35 +10,32 @@ void setup() {
     radio.initialise();
     delay(1000);
     float frequency = 0;
-    float startFreq = 96.4
-        radio.setFrequency(startFreq);//radio is an instanation of the radio module in the radio.h header file
+    float startFreq = 96.4;
+    radio.setFrequency(startFreq);    //radio is an instanation of the radio module in the radio.h header file
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
 }
 
-void frequencySwitch(float freq) {
-    //use radio frequency to restrict the frequency between a short range e.g 72.0 and 111.0
-    //lcd print "Station: " at 0,0
-    //lcd print frequency: at 0,1
-}
-
 void radio() {
     //when seek button pressed.
+    float frequency;
+    float newFrequency;
     while (button == "low") {
-        frequency += 0.1;
-        frequencySwitch(frequency);
+        if (frequency = maxFreq) {
+            frequency = startFrequency;
+        }
+        newFrequency = frequency + 0.1;
+        setFrequency(newFrequency);
+        frequency = newFrequency;
+        // display newFrequency
         delay(20);
-    }
-    if (maxFreq == frequency) {
-        frequency = startFrequency
-            frequencySwitch(startFrequency);
-        delay(20);
+
     }
     //when mem button pressed
     while (membutton1 == "low") {
-        frequency = 105.4
-            frequencySwitch(frequency);
+        frequency = 105.4;
+        setFrequency(frequency);
     }
 }
