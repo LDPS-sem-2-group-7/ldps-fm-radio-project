a702bc50219a99da5c981f62e919714c3e5dbfa4
Awes Mubarak <contact@awesmubarak.com>
2021-02-26 11:38:02 +0000

    Update frquency seeking to use built in seek function

--- skeleton.cpp/skeleton.cpp.ino
+++ skeleton.cpp/skeleton.cpp.ino
@@ -5,8 +5,8 @@
 
 AR1010 radio = AR1010();
 float frequency = 0;
-float startFreq = 96.4
-    int powerRadio = 0;//where 0 = off
+float startFreq = 96.4;
+int powerRadio = 0;//where 0 = off
 //we should declare component variables here.
 
 
@@ -18,27 +18,22 @@ void setup() {
     lcd.clear();
     radio.initialise();
     delay(1000);
-    radio.setFrequency(startFreq);//radio is an instanation of the radio module in the radio.h header file
-}
-
-void loop() {
-    // put your main code here, to run repeatedly:
+    radio.setFrequency(startFreq); //radio is an instanation of the radio module in the radio.h header file
+    // set constraiaint
 }
 
 void radio() {
     //when seek button pressed.
-    float frequency;
-    float newFrequency;
-    while (button == "low") {
-        if (frequency = maxFreq) {
+    frequency = constrain(frequency, 87.7, 107.9);
+    while (upButton == "low") {
+        if (frequency > maxFreq) {
             frequency = startFrequency;
         }
-        newFrequency = frequency + 0.1;
-        setFrequency(newFrequency);
-        frequency = newFrequency;
+        frequency = radio.seek('u');
         // display newFrequency
         delay(20);
     }
+
     //when mem button pressed
     while (membutton1 == "low") {
         frequency = 105.4;
