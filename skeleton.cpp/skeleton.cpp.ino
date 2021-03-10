ad449d544cd007869990aa7c321b007a0912900d
Awes Mubarak <contact@awesmubarak.com>
2021-03-10 13:59:18 +0000

    Added LCD writes, and updated frequency loop

--- skeleton.cpp/skeleton.cpp.ino
+++ skeleton.cpp/skeleton.cpp.ino
@@ -19,28 +19,59 @@ void setup() {
     radio.initialise();
     delay(1000);
     //the button pin pullup stuff
-    radio.setFrequency(startFreq); //radio is an instanation of the radio module in the radio.h header file
+    frequency = startFreq;
+    radio.setFrequency(frequency); //radio is an instanation of the radio module in the radio.h header file
     // set constraiaint
     lcd.setCursor(3, 0);//pixelx and pixely are placeholders for actual int pixel values when we know what we want.
     lcd.print("EAGLE RADIO");
-    lcd.setCursor();
+    lcd.setCursor(3, 1);
+    lcd.print(frequency);
+    //Serial.begin(9600); dont think we need this. Meant for clock
+}
+void loop() {
+    float minFreq = 87.7;
+    float maxFreq = 107.9;
+    frequency = constrain(frequency, minFreq, maxFreq);
+
+    while (digitalRead(freq up btn)) {
+        if (digitalRead(freq up)) {
+            while (digitalRead(freq down btn)) {
+                if (frequency > maxFreq) {
+                    frequency = minFrequency;
+                }
+                frequency = radio.seek('u');
+                frequencyUpdate(frequency);
+                // display newFrequency
+                delay(20);
+            }
+        } else if (digitalRead(freq down)) {    //not sure what button is this yet
+            while (downButton == "low") {
+                if (frequency < minFreq) {
+                    frequency = maxFreq;
+                }
+                frequency = radio.seek('d');
+                frequencyUpdate(frequency);
+                // display newFrequency
+                delay(20);
+            }
+        } else if (digitalRead(mem1)) {
+            while (digitalRead(freq up btn)) {
+                frequency = 96.4;
+                frequencyUpdate(frequency);
+            }
+        } else if (digitalRead(mem2)) {
+            while (digitalRead(freq up btn)) {
+              frequency = 100.0;
+              frequencyUpdate(frequency);
+            }
+        }
+    }
 }
 
-void radio() {
-    // when seek button pressed.
-    frequency = constrain(frequency, 87.7, 107.9);
-    while (upButton == "low") {
-        if (frequency > maxFreq) {
-            frequency = startFrequency;
-        }
-        frequency = radio.seek('u');
-        // display newFrequency
-        delay(20);
-    }
-
-    //when mem button pressed
-    while (membutton1 == "low") {
-        frequency = 105.4;
-        setFrequency(frequency);
-    }
+void frequencyUpdate(float frequency) {
+    // set the frequency
+    setFrequency(frequency);
+    // print to the LCD
+    lcd.setCursor(3, 1);
+    lcd.print(frequency);
 }
