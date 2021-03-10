c0dfb395a9ce34dfd8a28f7044a6a991a4d7b546
Awes Mubarak <contact@awesmubarak.com>
2021-03-10 13:15:33 +0000

    Clean code up a bit

--- LiquidCrystal_I2C.h
+++ LiquidCrystal_I2C.h
@@ -3,7 +3,7 @@
 #define LiquidCrystal_I2C_h
 
 #include <inttypes.h>
-#include "Print.h" 
+#include "Print.h"
 #include <Wire.h>
 
 // commands
@@ -52,80 +52,80 @@
 #define Rw B00000010  // Read/Write bit
 #define Rs B00000001  // Register select bit
 
-class LiquidCrystal_I2C : public Print {
+class LiquidCrystal_I2C: public Print {
 public:
-  LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows);
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS );
-  void clear();
-  void home();
-  void noDisplay();
-  void display();
-  void noBlink();
-  void blink();
-  void noCursor();
-  void cursor();
-  void scrollDisplayLeft();
-  void scrollDisplayRight();
-  void printLeft();
-  void printRight();
-  void leftToRight();
-  void rightToLeft();
-  void shiftIncrement();
-  void shiftDecrement();
-  void noBacklight();
-  void backlight();
-  void autoscroll();
-  void noAutoscroll(); 
-  void createChar(uint8_t, uint8_t[]);
-  void createChar(uint8_t location, const char *charmap);
-  // Example: 	const char bell[8] PROGMEM = {B00100,B01110,B01110,B01110,B11111,B00000,B00100,B00000};
-  
-  void setCursor(uint8_t, uint8_t); 
-#if defined(ARDUINO) && ARDUINO >= 100
-  virtual size_t write(uint8_t);
-#else
-  virtual void write(uint8_t);
-#endif
-  void command(uint8_t);
-  void init();
-  void oled_init();
+    LiquidCrystal_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows);
+    void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+    void clear();
+    void home();
+    void noDisplay();
+    void display();
+    void noBlink();
+    void blink();
+    void noCursor();
+    void cursor();
+    void scrollDisplayLeft();
+    void scrollDisplayRight();
+    void printLeft();
+    void printRight();
+    void leftToRight();
+    void rightToLeft();
+    void shiftIncrement();
+    void shiftDecrement();
+    void noBacklight();
+    void backlight();
+    void autoscroll();
+    void noAutoscroll();
+    void createChar(uint8_t, uint8_t[]);
+    void createChar(uint8_t location, const char *charmap);
+    // Example:     const char bell[8] PROGMEM = {B00100,B01110,B01110,B01110,B11111,B00000,B00100,B00000};
 
-////compatibility API function aliases
-void blink_on();						// alias for blink()
-void blink_off();       					// alias for noBlink()
-void cursor_on();      	 					// alias for cursor()
-void cursor_off();      					// alias for noCursor()
-void setBacklight(uint8_t new_val);				// alias for backlight() and nobacklight()
-void load_custom_character(uint8_t char_num, uint8_t *rows);	// alias for createChar()
-void printstr(const char[]);
+    void setCursor(uint8_t, uint8_t);
+    #if defined(ARDUINO) && ARDUINO >= 100
+    virtual size_t write(uint8_t);
+    #else
+    virtual void write(uint8_t);
+    #endif
+    void command(uint8_t);
+    void init();
+    void oled_init();
 
-////Unsupported API functions (not implemented in this library)
-uint8_t status();
-void setContrast(uint8_t new_val);
-uint8_t keypad();
-void setDelay(int,int);
-void on();
-void off();
-uint8_t init_bargraph(uint8_t graphtype);
-void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-	 
+    ////compatibility API function aliases
+    void blink_on();                    // alias for blink()
+    void blink_off();                       // alias for noBlink()
+    void cursor_on();                       // alias for cursor()
+    void cursor_off();                      // alias for noCursor()
+    void setBacklight(uint8_t new_val);         // alias for backlight() and nobacklight()
+    void load_custom_character(uint8_t char_num, uint8_t *rows); // alias for createChar()
+    void printstr(const char[]);
+
+    ////Unsupported API functions (not implemented in this library)
+    uint8_t status();
+    void setContrast(uint8_t new_val);
+    uint8_t keypad();
+    void setDelay(int, int);
+    void on();
+    void off();
+    uint8_t init_bargraph(uint8_t graphtype);
+    void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_col_end);
+    void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_col_end);
+
 
 private:
-  void init_priv();
-  void send(uint8_t, uint8_t);
-  void write4bits(uint8_t);
-  void expanderWrite(uint8_t);
-  void pulseEnable(uint8_t);
-  uint8_t _Addr;
-  uint8_t _displayfunction;
-  uint8_t _displaycontrol;
-  uint8_t _displaymode;
-  uint8_t _numlines;
-  bool _oled = false;
-  uint8_t _cols;
-  uint8_t _rows;
-  uint8_t _backlightval;
+    void init_priv();
+    void send(uint8_t, uint8_t);
+    void write4bits(uint8_t);
+    void expanderWrite(uint8_t);
+    void pulseEnable(uint8_t);
+    uint8_t _Addr;
+    uint8_t _displayfunction;
+    uint8_t _displaycontrol;
+    uint8_t _displaymode;
+    uint8_t _numlines;
+    bool _oled = false;
+    uint8_t _cols;
+    uint8_t _rows;
+    uint8_t _backlightval;
 };
 
 #endif
--- skeleton.cpp/skeleton.cpp.ino
+++ skeleton.cpp/skeleton.cpp.ino
@@ -8,7 +8,7 @@ float frequency = 0;
 float startFreq = 96.4;
 int powerRadio = 0;//where 0 = off
 //we should declare component variables here.
-
+LiquidCrystal_I2C lcd(0x27, 16, 2);//not sure about I2C address.
 
 void setup() {
     // put your setup code here, to run once:
@@ -18,12 +18,16 @@ void setup() {
     lcd.clear();
     radio.initialise();
     delay(1000);
+    //the button pin pullup stuff
     radio.setFrequency(startFreq); //radio is an instanation of the radio module in the radio.h header file
     // set constraiaint
+    lcd.setCursor(3, 0);//pixelx and pixely are placeholders for actual int pixel values when we know what we want.
+    lcd.print("EAGLE RADIO");
+    lcd.setCursor();
 }
 
 void radio() {
-    //when seek button pressed.
+    // when seek button pressed.
     frequency = constrain(frequency, 87.7, 107.9);
     while (upButton == "low") {
         if (frequency > maxFreq) {
