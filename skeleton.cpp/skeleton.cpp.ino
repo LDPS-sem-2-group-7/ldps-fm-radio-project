ebe1cd3656d73a948da900bc7b7a7f342255e521
Awes Mubarak <contact@awesmubarak.com>
2021-03-11 17:02:42 +0000

    Clean up code a bit, start rewriting to support rotary encoder

--- skeleton.cpp/skeleton.cpp.ino
+++ skeleton.cpp/skeleton.cpp.ino
@@ -2,76 +2,91 @@
 #include "LiquidCrystal_I2C.h"
 #include <Wire.h>
 
+// creating the radio and LCD objects
+AR1010 g_radio = AR1010();
+LiquidCrystal_I2C g_lcd(0x27, 16, 2);//g_ corresponds to global.
 
-AR1010 radio = AR1010();
-float frequency = 0;
-float startFreq = 96.4;
-int powerRadio = 0;//where 0 = off
-//we should declare component variables here.
-LiquidCrystal_I2C lcd(0x27, 16, 2);//not sure about I2C address.
+// global variables
+const float c_minFreq = 96.4;//c_ corresponds to constant.
+const float c_maxFreq = 107.9;
 
 void setup() {
-    // put your setup code here, to run once:
+    // initialise the objects
     Wire.begin();
-    lcd.begin();
-    lcd.backlight();
-    lcd.clear();
+    g_lcd.begin();
     radio.initialise();
     delay(1000);
-    //the button pin pullup stuff
-    frequency = startFreq;
-    radio.setFrequency(frequency); //radio is an instanation of the radio module in the radio.h header file
-    // set constraiaint
-    lcd.setCursor(3, 0);//pixelx and pixely are placeholders for actual int pixel values when we know what we want.
-    lcd.print("EAGLE RADIO");
-    lcd.setCursor(3, 1);
-    lcd.print(frequency);
+    // set the frequency
+    radio.setFrequency(c_minFreq);
+    // setup the glcd
+    g_lcd.backlight();
+    g_lcd.clear();
+    g_lcd.setCursor(3, 0);
+    g_lcd.print("EAGLE RADIO");
+    g_lcd.setCursor(3, 1);
+    g_lcd.print(c_minFreq);
+
     //Serial.begin(9600); dont think we need this. Meant for clock
 }
-void loop() {
-    float minFreq = 87.7;
-    float maxFreq = 107.9;
-    frequency = constrain(frequency, minFreq, maxFreq);
 
-    while (digitalRead(freq up btn)) {
-        if (digitalRead(freq up)) {
-            while (digitalRead(freq down btn)) {
-                if (frequency > maxFreq) {
-                    frequency = minFrequency;
-                }
-                frequency = radio.seek('u');
-                frequencyUpdate(frequency);
-                // display newFrequency
-                delay(20);
-            }
-        } else if (digitalRead(freq down)) {    //not sure what button is this yet
-            while (downButton == "low") {
-                if (frequency < minFreq) {
-                    frequency = maxFreq;
-                }
-                frequency = radio.seek('d');
-                frequencyUpdate(frequency);
-                // display newFrequency
-                delay(20);
-            }
-        } else if (digitalRead(mem1)) {
-            while (digitalRead(freq up btn)) {
-                frequency = 96.4;
-                frequencyUpdate(frequency);
-            }
-        } else if (digitalRead(mem2)) {
-            while (digitalRead(freq up btn)) {
-              frequency = 100.0;
-              frequencyUpdate(frequency);
-            }
-        }
+void loop() {
+    // set frequency constraints
+    float frequency;
+    frequency = constrain(frequency, c_minFreq, c_maxFreq);
+
+    // read button states
+    int memButton1State = digitalRead(MEM BUTTON 1 PIN);
+    int memButton2State = digitalRead(MEM BUTTON 2 PIN);
+
+    // memory buttons
+    if (memButton1State == HIGH) {
+        frequencyUpdate(c_minFreq);
     }
+
+    if (memButton2State == HIGH) {
+        frequencyUpdate(c_maxFreq);
+    }
+
+
+    // while (digitalRead(freq up btn)) {//remove while loops since it is a loop() function.
+    //     if (digitalRead(freq up)) {
+    //         while (digitalRead(freq down btn)) {
+    //             if (frequency > c_maxFreq) {
+    //                 frequency = c_minFrequency;
+    //             }
+    //             frequency = radio.seek('u');
+    //             frequencyUpdate(frequency);
+    //             // display newFrequency
+    //             delay(20);
+    //         }
+    //     } else if (digitalRead(freq down)) {    //not sure what button is this yet
+    //         while (downButton == "low") {
+    //             if (frequency < c_minFreq) {
+    //                 frequency = c_maxFreq;
+    //             }
+    //             frequency = radio.seek('d');
+    //             frequencyUpdate(frequency);
+    //             // display newFrequency
+    //             delay(20);
+    //         }
+    //     } else if (digitalRead(mem1)) {
+    //         while (digitalRead(freq up btn)) {
+    //             frequency = 96.4;
+    //             frequencyUpdate(frequency);
+    //         }
+    //     } else if (digitalRead(mem2)) {
+    //         while (digitalRead(freq up btn)) {
+    //             frequency = 100.0;
+    //             frequencyUpdate(frequency);
+    //         }
+    //     }
+    // }
 }
 
 void frequencyUpdate(float frequency) {
     // set the frequency
-    setFrequency(frequency);
+    g_radio.setFrequency(frequency);
     // print to the LCD
-    lcd.setCursor(3, 1);
-    lcd.print(frequency);
+    g_lcd.setCursor(3, 1);
+    g_lcd.print(frequency);
 }
