94f1362586808025c9e64985a5af918882e38076
Sushant-Misra <65679953+Sushant-Misra@users.noreply.github.com>
2021-03-17 15:31:32 +0000

    Update constants.h
    Corrected the digital pin numbers basis the schematic. The software team can come up with appropriate variable names for the buttons concerning the clock module. Have added comments for which pins are meant for this module.

--- constants.h
+++ constants.h
@@ -9,15 +9,17 @@ const float c_memFreq2 = 101.0;
 const int c_reDat = 3;
 const int c_reClk = 2;
 const int c_reSw = 4;
-const int c_memButton1Pin 8;//TODO:header file for the constants
-const int c_memButton2Pin 9;
-const int c_frequButton1Pin 6;
-const int c_frequButton2Pin 7;
+const int c_memButton1Pin 5;//TODO:header file for the constants
+const int c_memButton2Pin 6;
+const int c_frequButton1Pin 9;
+const int c_frequButton2Pin 10;
+//Use pin buttons D7 and D8 to adjust RTC module
 const int c_rtcPinSDA 27; // this is just the i2c rail
 const int c_rtcPinSCL 28;
 const int c_lcdLen 16;
 const int c_lcdHeight 2;
 const int c_minVolume 0;
 const int c_maxVolume 18;
+
 
 #endif
