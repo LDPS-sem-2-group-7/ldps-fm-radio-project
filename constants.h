a9d68e5beb217e918f0a452364962b769d693a47
Robert Vasistha <78962360+robvasistha@users.noreply.github.com>
2021-03-17 14:24:41 +0000

    Created a header file to store constants used

---
+++ constants.h
@@ -0,0 +1,23 @@
+#ifndef constants_h
+#define constants_h
+
+const int c_i2cDataPath 0x27;
+const float c_minFreq = 96.4;
+const float c_maxFreq = 107.9;
+const float c_memFreq1 = 100.0; // TODO fix
+const float c_memFreq2 = 101.0;
+const int c_reDat = 3;
+const int c_reClk = 2;
+const int c_reSw = 4;
+const int c_memButton1Pin 8;//TODO:header file for the constants
+const int c_memButton2Pin 9;
+const int c_frequButton1Pin 6;
+const int c_frequButton2Pin 7;
+const int c_rtcPinSDA TODO;///////////
+const int c_rtcPinSCL TODO;
+const int c_lcdLen 16;
+const int c_lcdHeight 2;
+const int c_minVolume 0;
+const int c_maxVolume 18;
+
+#endif
