7a5079b96925d41984843a3ef185139b6e657fb4
robvasistha <78962360+robvasistha@users.noreply.github.com>
2021-03-24 09:11:12 +0000

    Updated constants.h with placeholders for vol btns

--- constants.h
+++ constants.h
@@ -13,6 +13,8 @@ const int c_memButton1Pin 5;//TODO:header file for the constants
 const int c_memButton2Pin 6;
 const int c_frequButton1Pin 9;
 const int c_frequButton2Pin 10;
+//const int c_volUp #some number;
+//const int c_volDown #some number;
 //Use pin buttons D7 and D8 to adjust RTC module
 const int c_rtcPinSDA 27; // this is just the i2c rail
 const int c_rtcPinSCL 28;
